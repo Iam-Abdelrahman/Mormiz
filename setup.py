ea56137b32285d5912e7dfb237414317a6a7c47a
a3bd2lra7man <a3bd2llah@gmail.com>
2024-07-02 14:49:56 +0300

    ok

--- setup.py
+++ setup.py
@@ -14,4 +14,24 @@ setup(
     ],
     packages=["mormiz"],
     zip_safe=False,
+    version = '0.1',
+    license='MIT',  
+    description = 'Mormiz an Arabic tokenizer for LLM',    
+    author = 'Iam-Abdelrahman',
+    author_email = 'iamabdelrahmanai@gmail.com',   
+    url = 'https://github.com/Iam-Abdelrahman/Mormiz',
+    download_url = "https://github.com/Iam-Abdelrahman/Mormiz/archive/refs/tags/0.1.tar.gz",
+    install_requires=[
+        'setuptools_rust',
+        'setuptools'
+    ],
+    classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'License :: OSI Approved :: MIT License',   
+    # 'Programming Language :: Python :: 3',   
+    # 'Programming Language :: Python :: 3.4',
+    # 'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.11',
+  ],
 )
