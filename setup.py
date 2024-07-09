9c1b339a0fb94afdf8947fc0ca987fd229df1cd3
a3bd2lra7man <a3bd2llah@gmail.com>
2024-07-09 14:03:41 +0300

    fix names issue

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="mormiz",
     rust_extensions=[
         RustExtension(
-            "mormiz._mormiz",
+            "mormiz.mormiz",
             binding=Binding.PyO3,
             # Between our use of editable installs and wanting to use Rust for performance sensitive
             # code, it makes sense to just always use --release
